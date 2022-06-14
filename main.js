20af82cabb775651380e21e025501bf267e76c42
sergnio <sergnioa@gmail.com>
2022-06-14 18:44:10 -0500

    add update statement

--- main.js
+++ main.js
@@ -3,19 +3,45 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+async function updateSergio() {
+  await prisma.user.update({
+    where: {
+      id: "62a91a54723f12f84d207875",
+    },
+    data: {
+      activities: {
+        createMany: {
+          data: [
+            {
+              name: "Opening up youtube",
+              timesDone: 5,
+            },
+            {
+              name: "Taking out the trash",
+              timesDone: 2,
+            },
+            {
+              name: "Feeding the kitties",
+              timesDone: 2,
+            },
+          ],
+        },
+      },
+    },
+  });
+}
+
 async function main() {
   console.log("connecting");
   // Connect the client
   await prisma.$connect();
+  // await updateSergio();
 
-  await prisma.user.create({
-    data: {
-      name: "Sergio",
-      email: "sergnio@gmail.com",
+  const allUsers = await prisma.user.findMany({
+    include: {
+      activities: true,
     },
   });
-
-  const allUsers = await prisma.user.findMany();
   console.log(allUsers);
 }
 
